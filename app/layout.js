25923d78c57183e7615179c616dfc946c785daf7
Ilhan Basic <ilhanbasic456@gmail.com>
2025-03-02 14:22:07 +0100

    added script into head tag

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4844344743214501"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
